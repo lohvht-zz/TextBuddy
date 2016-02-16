4f6e2a210b6ba3b9d36d45b1eafa12f932164a9a
Loh Victor <Loh Victor>
2016-02-16 22:18:51 +0800

    added a search funtionality, also cleaned out some magic strings by refactoring them into constants. As a direct consequence of this, more promptToUser functions have been overloaded. Several minor changes in names were also done.

--- TextBuddy.java
+++ TextBuddy.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
@@ -48,6 +49,7 @@ class lineCompare implements Comparator<String>{
 }
 class TextBuddy {
 	private static final String NAME_TEMP_FILE = "editedTemp.txt";
+	//MESSAGES
 	private static final String MESSAGE_COMMAND_PROMPT = "Command: ";
 	private static final String MESSAGE_HELP_SHEET = "add <TEXT>        --adds the text to the file currently open\n"
 			+ "delete <LINENUM>        --removes the text written on that line\n"
@@ -55,19 +57,24 @@ class TextBuddy {
 			+ "this DOES NOT save the document\n" + "save        --saves all changes made to the text document\n"
 			+ "exit        --saves the document first before exiting Text Buddy\n"
 			+ "clear		--wipes and removes all the data currently stored in the text file, THIS ACTION IS NOT REVERSIBLE\n"
-			+ "search		--sorts the current list line by line in lexicographical order," ;
+			+ "search		--sorts the current list line by line in alphabetical order.\n" ;
 	private static final String MESSAGE_FILE_READY = "Welcome to Text Buddy, %1$s is ready for use!\n";
 	private static final String MESSAGE_CLEAR_TEXT_MESSAGE = "all contents deleted from %1$s\n";
-	private static final String MESSAGE_FILE_IS_EMPTY = "%1$s is empty";
-	private static final String MESSAGE_LIST_SORTED = "%1$s is sorted, type display to show the updated list";
+	private static final String MESSAGE_FILE_IS_EMPTY = "%1$s is empty\n";
+	private static final String MESSAGE_LIST_SORTED = "%1$s is sorted, type \"display\" to show the updated list\n";
+	private static final String MESSAGE_RETRIEVED_SEARCHED_WORD_INDEX = "search results for the word \"%1$s\"\n";
+	private static final String MESSAGE_LINE_PRINTED = "%1$d. %2$s\n";
+	private static final String MESSAGE_STRING_SUCCESSFULLY_ADDED = "added to %1$s: \"%2$s\"\n";
+	private static final String MESSAGE_TEXT_DOCUMENT_IS_EMPTY = "%1$s is empty\n";
+	//ERROR MESSAGES
 	private static final String ERROR_FILE_REPLACEMENT = "Something has gone wrong with file replacement";
 	private static final String ERROR_NO_FILE_STRING_DETECTED = "No file input stated, terminating program";
 	private static final String ERROR_DELETE_FORMAT_INCORRECT = "Wrong format for delete, parameter should be a number";
-	private static final String ERROR_COULD_NOT_RENAME_FILE = "Could not rename file";
+	private static final String ERROR_COULD_NOT_RENAME_FILE = "could not rename file";
 	private static final String ERROR_COMMAND_INVALID = "%1$s is not valid, please try again, enter <help> without the "
 			+ "arrow brackets to get help on commands\n";
-	private static final String ERROR_LINE_NUMBER_DOES_NOT_EXIST = "line number %1$s does not exist";
-	
+	private static final String ERROR_LINE_NUMBER_DOES_NOT_EXIST = "line number %1$s does not exist\n";
+	private static final String ERROR_SEARCHED_WORD_IS_NOT_FOUND = "Searched word %1$s cannot be found on the text document\n";
 	
 	private static boolean isRunning = true;
 	private static Scanner inputScanner = new Scanner(System.in);
@@ -86,6 +93,7 @@ class TextBuddy {
 		promptToUser(MESSAGE_FILE_READY, fileName);
 
 		LinkedList<String> tempContents = new LinkedList<String>();
+		
 		BufferedReader fileReader = createReader(fileName);
 		BufferedWriter tempFileWriter = createWriter(NAME_TEMP_FILE);
 		copyFileContentToTempContents(fileReader, tempContents);
@@ -97,55 +105,60 @@ class TextBuddy {
 			String command = getCommandString(input);
 
 			switch (command) {
-			case "add":
-				addCommand(tempContents, input);
-				saveAndWriteToFile(tempContents, tempFileWriter, fileReader);
-				fileReader = createReader(fileName);
-				tempFileWriter = createWriter(NAME_TEMP_FILE);
-				copyFileContentToTempContents(fileReader, tempContents);
-				break;
-
-			case "display":
-				displayCommand(tempContents);
-				break;
-
-			case "delete":
-				deleteCommand(tempContents, input);
+				case "add":
+					addCommand(tempContents, input);
+					
+					saveAndWriteToFile(tempContents, tempFileWriter, fileReader);
+					fileReader = createReader(fileName);
+					tempFileWriter = createWriter(NAME_TEMP_FILE);
+					copyFileContentToTempContents(fileReader, tempContents);
+					break;
+	
+				case "display":
+					displayCommand(tempContents);
+					break;
+	
+				case "delete":
+					deleteCommand(tempContents, input);
+					
+					saveAndWriteToFile(tempContents, tempFileWriter, fileReader);
+					fileReader = createReader(fileName);
+					tempFileWriter = createWriter(NAME_TEMP_FILE);
+					copyFileContentToTempContents(fileReader, tempContents);
+					break;
+	
+				case "exit":
+					setRunning();
+					saveAndWriteToFile(tempContents, tempFileWriter, fileReader);
+					break;
+	
+				case "clear":
+					clearCommand(tempContents);
+					
+					saveAndWriteToFile(tempContents, tempFileWriter, fileReader);
+					fileReader = createReader(fileName);
+					tempFileWriter = createWriter(NAME_TEMP_FILE);
+					break;
+	
+				case "help":
+					promptToUser(MESSAGE_HELP_SHEET);
+					break;
 				
-				saveAndWriteToFile(tempContents, tempFileWriter, fileReader);
-				fileReader = createReader(fileName);
-				tempFileWriter = createWriter(NAME_TEMP_FILE);
-				copyFileContentToTempContents(fileReader, tempContents);
-				break;
-
-			case "exit":
-				setRunning();
-				saveAndWriteToFile(tempContents, tempFileWriter, fileReader);
-				break;
-
-			case "clear":
-				clearCommand(tempContents);
+				case "sort":
+					sortCommand(tempContents);
+					saveAndWriteToFile(tempContents, tempFileWriter, fileReader);
+					fileReader = createReader(fileName);
+					tempFileWriter = createWriter(NAME_TEMP_FILE);
+					copyFileContentToTempContents(fileReader, tempContents);
+					break;
 				
-				saveAndWriteToFile(tempContents, tempFileWriter, fileReader);
-				fileReader = createReader(fileName);
-				tempFileWriter = createWriter(NAME_TEMP_FILE);
-				break;
-
-			case "help":
-				promptToUser(MESSAGE_HELP_SHEET);
-				break;
-			
-			case "sort":
-				sortAlphabetically(tempContents);
-				saveAndWriteToFile(tempContents, tempFileWriter, fileReader);
-				fileReader = createReader(fileName);
-				tempFileWriter = createWriter(NAME_TEMP_FILE);
-				copyFileContentToTempContents(fileReader, tempContents);
-				break;
-
-			default:
-				promptToUser(ERROR_COMMAND_INVALID, command);
-				break;
+				case "search":
+					searchCommand(tempContents, input);
+					break;
+					
+				default:
+					promptToUser(ERROR_COMMAND_INVALID, command);
+					break;
 			}
 		}
 		inputScanner.close();
@@ -170,7 +183,15 @@ class TextBuddy {
 	public static void promptToUser(String message, String name){
 		System.out.printf(message, name);
 	}
-
+	
+	public static void promptToUser(String message, int number, String name){
+		System.out.printf(message, number, name);
+	}
+	
+	public static void promptToUser(String message, String name1, String name2){
+		System.out.printf(message, name1, name2);
+	}
+	
 	private static File createFile(String fileName) {
 		File file = new File(fileName);
 		try {
@@ -221,17 +242,17 @@ class TextBuddy {
 	private static void addCommand(LinkedList<String> tempContents, String[] input) {
 		String textToBeAdded = getUserResponse(input);
 		tempContents.add(textToBeAdded);
-		System.out.println("added to " + fileName + ": \"" + textToBeAdded + "\"");
+		promptToUser(MESSAGE_STRING_SUCCESSFULLY_ADDED, fileName, textToBeAdded);
 	}
 
 	private static void displayCommand(LinkedList<String> tempContents) {
 		if (getTotalNumberOfLines(tempContents) == 0) {
-			System.out.println(fileName + " is empty");
+			promptToUser(MESSAGE_TEXT_DOCUMENT_IS_EMPTY, fileName);
 			return;
 		}
 		int counter = 1;
 		for (String lineContent : tempContents) {
-			System.out.println(counter + ". " + lineContent);
+			promptToUser(MESSAGE_LINE_PRINTED, counter, lineContent);
 			counter++;
 		}
 	}
@@ -256,10 +277,63 @@ class TextBuddy {
 		}
 	}
 	
-	public static void sortAlphabetically(LinkedList<String> tempContents){
+	public static void sortCommand(LinkedList<String> tempContents){
 		lineCompare lineComparator = new lineCompare();
 		Collections.sort(tempContents, lineComparator);
 		promptToUser(MESSAGE_LIST_SORTED, fileName);
+	}
+	
+	public static boolean isContainSubstring(String sourceString, String substring) {
+	    int substringLength = getStringLength(substring);
+	    
+	    if (substringLength == 0){
+	        return true;
+	    }
+	        
+	    char subStringFirstLowerCaseChar = Character.toLowerCase(substring.charAt(0));
+	    char subStringFirstUpperCaseChar = Character.toUpperCase(substring.charAt(0));
+
+	    for (int i = getStringLength(sourceString) - substringLength; i >= 0; i--) {
+	        char sourceCharacterAt = sourceString.charAt(i);
+	        
+	        if (sourceCharacterAt != subStringFirstLowerCaseChar && sourceCharacterAt != subStringFirstUpperCaseChar){
+	            continue;
+	        }
+	        
+	        if (sourceString.regionMatches(true, i, substring, 0, substringLength)){
+	            return true;
+	        }
+	    }
+
+	    return false;
+	}
+	
+	public static void searchCommand(LinkedList<String> tempContents, String[] input){
+		ArrayList<Integer> indexOfWordInstanceFound = new ArrayList<Integer>();
+		String wordToSearchFor = getUserResponse(input);
+		
+		int listSize = getTotalNumberOfLines(tempContents);
+		
+		for(int i = 0; i < listSize; i++){
+			String lineString = tempContents.get(i);
+			if(isContainSubstring(lineString, wordToSearchFor)){
+				indexOfWordInstanceFound.add(i);
+			}
+		}
+		if(indexOfWordInstanceFound.isEmpty()){
+			promptToUser(ERROR_SEARCHED_WORD_IS_NOT_FOUND, wordToSearchFor);
+		} else {
+			promptToUser(MESSAGE_RETRIEVED_SEARCHED_WORD_INDEX, wordToSearchFor);
+			for(int j : indexOfWordInstanceFound){
+				int counter = 1+j;
+				String lineContent = tempContents.get(j);
+				promptToUser(MESSAGE_LINE_PRINTED, counter, lineContent);
+			}
+		}	
+	}
+	
+	public static int getStringLength(String string) {
+		return string.length();
 	}
 	
 	public static String getLineRemoved(LinkedList<String> tempContents, int tempContentsIndexToRemove) {
